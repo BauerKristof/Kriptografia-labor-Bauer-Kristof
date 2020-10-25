5999301405ac4b94eb3d5047a2ef1ffea931cfc1
Bauer Kristof <bauerkristof99@gmail.com>
2020-10-25 14:07:32 +0200

    Lab1 - Caeser+Viginere Encrypt/Decrypt done + test functions

--- lab1/assign1/crypto.py
+++ lab1/assign1/crypto.py
@@ -1,16 +1,16 @@
 """Assignment 1: Cryptography for CS41 Winter 2020.
 
-Name: <YOUR NAME>
-SUNet: <SUNet ID>
+Name: Bauer Kristof
+SUNet: bkim1790
 
-Replace this placeholder text with a description of this module.
+Lab1 Implementation
 """
-import utils
-
+import string
 
 #################
 # CAESAR CIPHER #
 #################
+
 
 def encrypt_caesar(plaintext):
     """Encrypt a plaintext using a Caesar cipher.
@@ -22,8 +22,22 @@ def encrypt_caesar(plaintext):
 
     :returns: The encrypted ciphertext.
     """
+
     # Your implementation here.
-    raise NotImplementedError('encrypt_caesar is not yet implemented!')
+    charList = []
+    charList[:] = plaintext
+    ASCII_UPPERS = string.ascii_uppercase
+    encryptedText = ""
+
+    for i in charList:
+        if not i.isspace():
+            if i in string.ascii_uppercase:
+                encryptedText += ASCII_UPPERS[(ASCII_UPPERS.index(i)+3) % 26]
+            else:
+                encryptedText += i
+        else:
+            encryptedText += i
+    return encryptedText
 
 
 def decrypt_caesar(ciphertext):
@@ -37,7 +51,50 @@ def decrypt_caesar(ciphertext):
     :returns: The decrypted plaintext.
     """
     # Your implementation here.
-    raise NotImplementedError('decrypt_caesar is not yet implemented!')
+    charList = []
+    charList[:] = ciphertext
+    ASCII_UPPERS = string.ascii_uppercase
+    decryptedText = ""
+
+    for i in charList:
+        if not i.isspace():
+            if i in string.ascii_uppercase:
+                decryptedText += ASCII_UPPERS[(ASCII_UPPERS.index(i)-3) % 26]
+            else:
+                decryptedText += i
+        else:
+            decryptedText += i
+    return decryptedText
+
+
+def testCaesar():
+    print("Encrypt-Caesar\n")
+    print(encrypt_caesar("A"))
+    print(encrypt_caesar("B"))
+    print(encrypt_caesar("I"))
+    print(encrypt_caesar("X"))
+    print(encrypt_caesar("Z"))
+    print(encrypt_caesar("AA"))
+    print(encrypt_caesar("TH"))
+    print(encrypt_caesar("CAT"))
+    print(encrypt_caesar("DOG"))
+    print(encrypt_caesar("TOO"))
+    print(encrypt_caesar("DAMN"))
+    print(encrypt_caesar("DANIEL"))
+    print(encrypt_caesar("WHEEEEEE"))
+    print(encrypt_caesar("WITH SPACE"))
+    print(encrypt_caesar("WITH TWO SPACES"))
+    print(encrypt_caesar("NUM83R5"))
+    print(encrypt_caesar("0DD !T$"))
+
+    print("\nDecrpyt Caesar\n")
+    print(decrypt_caesar("0GG !W$"))
+    print(decrypt_caesar("QXP83U5"))
+    print(decrypt_caesar("ZLWK WZR VSDFHV"))
+    print(decrypt_caesar("ZLWK VSDFH"))
+
+
+# testCaesar()
 
 
 ###################
@@ -57,7 +114,32 @@ def encrypt_vigenere(plaintext, keyword):
     :returns: The encrypted ciphertext.
     """
     # Your implementation here.
-    raise NotImplementedError('encrypt_vigenere is not yet implemented!')
+    textList = []
+    codeList = []
+    textList[:] = plaintext
+    codeList[:] = keyword
+    orderOfText = []
+    orderOfKeyword = []
+    encryptedText = ""
+
+    for i in textList:
+        orderOfText.append(ord(i))
+
+    for i in codeList:
+        orderOfKeyword.append(ord(i))
+
+    lengthOfKeyword = len(orderOfKeyword)
+    print(orderOfText)
+    print(orderOfKeyword)
+
+    for i in range(0, len(orderOfText), 1):
+        orderOfEncryptedChar = (
+            orderOfText[i] + orderOfKeyword[i % lengthOfKeyword])
+        abcOrderOfChar = orderOfEncryptedChar % 26
+        asciiValueOfEncrpytedChar = chr(abcOrderOfChar+65)
+        encryptedText += asciiValueOfEncrpytedChar
+
+    return encryptedText
 
 
 def decrypt_vigenere(ciphertext, keyword):
@@ -73,12 +155,55 @@ def decrypt_vigenere(ciphertext, keyword):
     :returns: The decrypted plaintext.
     """
     # Your implementation here.
-    raise NotImplementedError('decrypt_vigenere is not yet implemented!')
+    textList = []
+    codeList = []
+    textList[:] = ciphertext
+    codeList[:] = keyword
+    orderOfText = []
+    orderOfKeyword = []
+    decryptedText = ""
 
+    for i in textList:
+        orderOfText.append(ord(i))
+
+    for i in codeList:
+        orderOfKeyword.append(ord(i))
+
+    lengthOfKeyword = len(orderOfKeyword)
+
+    for i in range(0, len(orderOfText), 1):
+        orderOfEncryptedChar = (
+            orderOfText[i] - orderOfKeyword[i % lengthOfKeyword])
+        abcOrderOfChar = orderOfEncryptedChar % 26
+        asciiValueOfEncrpytedChar = chr(abcOrderOfChar+65)
+        decryptedText += asciiValueOfEncrpytedChar
+
+    return decryptedText
+
+
+def testVigenere():
+    print("Encrypt-Vigenere\n")
+    print(encrypt_vigenere("FLEEATONCE", "A"))
+    print(encrypt_vigenere("IMHIT", "H"))
+    print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
+    print(encrypt_vigenere("WEAREDISCOVERED", "LEMON"))
+    print(encrypt_vigenere("SHORTERKEY", "XYZZYZ"))
+    print(encrypt_vigenere("A", "ONEINPUT"))
+    print("\nDecrypt-Vigenere\n")
+    print(decrypt_vigenere("FLEEATONCE", "A"))
+    print(decrypt_vigenere("PTOPA", "H"))
+    print(decrypt_vigenere("LXFOPVEFRNHR", "LEMON"))
+    print(decrypt_vigenere("HIMFROMEQBGIDSQ", "LEMON"))
+    print(decrypt_vigenere("PFNQRDOIDX", "XYZZYZ"))
+    print(decrypt_vigenere("O", "ONEINPUT"))
+
+
+# testVigenere()
 
 ########################################
 # MERKLE-HELLMAN KNAPSACK CRYPTOSYSTEM #
 ########################################
+
 
 def generate_private_key(n=8):
     """Generate a private key to use with the Merkle-Hellman Knapsack Cryptosystem.
